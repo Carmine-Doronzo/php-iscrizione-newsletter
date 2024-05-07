678d17d6d21c4d3111fc6b3193fdc3d75161d1cd
Carmine-Doronzo <carmine.doronzo92@gmail.com>
2024-05-07 16:03:31 +0200

    Fix with booltip #1

--- index.php
+++ index.php
@@ -1,31 +1,55 @@
+<?php
+include __DIR__ . '/utilities.php';
 
-<?php 
 
-   include __DIR__ . '/utilities.php'
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <title>PHP iscrizione newsletter</title>
 </head>
+
 <body>
 
     <main>
 
         <form action="" method="POST">
             <!-- ho messo un input di tipo text perchè l'input di tipo email fa già il controllo sulla @ e il .  -->
-            <label for="email">Email</label>
-            <input type="text" name="email" id="" placeholder="Inserisci la tua email">
-            <input type="submit">
+            <!-- <label for="email">Email</label> -->
+            <!-- <input type="text" name="email" id="" placeholder="Inserisci la tua email"> -->
+            <div class="input-group mb-3">
+                <span class="input-group-text" id="basic-addon1">@</span>
+                <input type="text" class="form-control" placeholder="Inserisci email" name="email" aria-label="Username" aria-describedby="basic-addon1">
+            </div>
+
+            <input type="submit" class="btn btn-primary">
         </form>
 
-        <p> <?php echo getEmail() ?></p>
+        <p>
+            <?php
+            if (getEmail() === true) {
+            ?>
+        <div class="alert alert-success" role="alert">
+            Accesso garantito
+        </div>
+    <?php
+            } else {
+    ?>
+        <div class="alert alert-danger" role="alert">
+            Accesso negato
+        </div>
+    <?php
+            }
+    ?></p>
 
     </main>
-    
+
 </body>
+
 </html>
--- utilities.php
+++ utilities.php
@@ -1,18 +1,29 @@
-<?php 
+<?php
 
-    function getEmail(){
-        
-        $email = $_POST['email'] ?? null;
-        //var_dump($email);
-        
-        if(str_contains($email,'@') && str_contains($email,'.')){
-            echo '<p>accesso garantito</p>';
-        }else{
-            echo '<p>acesso negato</p>';
-        }
+function getEmail()
+{
 
-        return $email;
-    
-    }
+    $email = $_POST['email'] ?? null;
     //var_dump($email);
-?>
+    $event = null;
+    
+
+    if (str_contains($email, '@') && str_contains($email, '.')) {
+        // echo '
+        //     <div class="alert alert-success" role="alert">
+        //         Accesso garantito
+        //     </div>
+        // ';
+        return $event = true;
+    } else {
+        // echo '
+        //     <div class="alert alert-danger" role="alert">
+        //         Accesso negato
+        //     </div>
+        // ';
+        return $event = false;
+    }
+
+    
+}
+//var_dump($email);
